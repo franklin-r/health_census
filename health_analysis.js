6ef8df4cd9e09b2cb3f8873d50985aba09c6ddd0
franklin-r <alexis.rossi97@gmail.com>
2025-04-09 15:22:02 -0400

    Add addPatient() function

---
+++ health_analysis.js
@@ -0,0 +1,22 @@
+const addPatientButton = document.getElementById("addPatient");
+const report = document.getElementById("report");
+const btnSearch = document.getElementById('btnSearch');
+const patients = [];
+
+function addPatient() {
+    const name      = document.querySelector("#name").value;
+    const gender    = document.querySelector('input[name="gender"]:checked');
+    const age       = document.querySelector("#age").value;
+    const condition = document.querySelector("#condition").value;
+
+    if (name && gender && age && condition) {
+        patients.push({
+            name: name,
+            gender: gender.value,
+            age: age,
+            condition: condition
+        });
+        resetForm();
+        generateReport();
+    }
+}
